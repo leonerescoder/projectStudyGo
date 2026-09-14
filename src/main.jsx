d1e5ff95ce0bc73c59285babb18c8831f21f9608
gabriellvieiraa <gabrieldicassiovieira@gmail.com>
2026-09-14 20:20:06 -0300

    tela categoria

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+<<<<<<< Updated upstream
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './globals.css'
@@ -9,5 +10,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+=======
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+>>>>>>> Stashed changes
   </React.StrictMode>,
 )
