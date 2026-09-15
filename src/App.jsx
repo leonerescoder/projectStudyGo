a4d90ef8b62dd12dc026bd3791b9c3404565beda
Nicolas <nicolashenriquec583@gmail.com>
2026-09-14 22:02:46 -0300

    tela companie
    companie corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
 import Course from './pages/Course/Course';
-<<<<<<< Updated upstream
 import CoursesCatalog from './pages/CoursesCatalog/CoursesCatalog';
-=======
 import Escolas from './pages/Escolas/Escolas';
 import EscolaSelecionada from './pages/Escolas/EscolaSelecionada';
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -16,12 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course/:id" element={<Course />} />
-<<<<<<< Updated upstream
         <Route path="/cursos" element={<CoursesCatalog />} />
-=======
         <Route path="/escolas" element={<Escolas />} />
         <Route path="/escolas/:id" element={<EscolaSelecionada />} />
->>>>>>> Stashed changes
       </Routes>
     </>
   );
